06a0d77de26011254614acc36014e45416ebb40c
Trevor <trevor@trevorbest.com>
2025-05-11 18:12:13 -0500

    Revert "feat: Add Match Score Entry components"
    This reverts commit f5d0ab44a41e0aca59547a26366462200dc8cbc1.

--- frontend/src/components/pages/MatchScoreEntry/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MatchHeader } from './MatchHeader';
-export { default as MatchScorecard } from './MatchScorecard';
-export { default as MatchResults } from './MatchResults';
-export { default as SubstituteDialog } from './SubstituteDialog';
